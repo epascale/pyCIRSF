fe25f6b761efdbce56aa6237763d0ede1e704534
Lorenzo Mugnai <mastro.mugna@gmail.com>
2018-01-15 11:02:39 +0100

    photometry code: changes for output and bad pixels in apertures
    output destination edited (to gdrive dir), added photom in output, interpolation for bad pixels in apertures.

--- photometry/photom_v1.py
+++ photometry/photom_v1.py
@@ -10,7 +10,7 @@ from scipy.stats import gaussian_kde
 from scipy.optimize import minimize_scalar
 
 import pyCIRSF as irsf
-import os, glob, sys, mylib
+import os, glob, sys#, mylib
 
 
 #######################################################
@@ -52,6 +52,7 @@ def photom(ima, pos, radius, r_in=None, r_out=None, method='median'):
       bg_ima = annulus_mask.cutout(ima_)
       bg_mask = annulus_mask.cutout(mask_.astype(np.int))
       bg_ima = np.ma.array(bg_ima, mask= bg_mask.astype(np.bool) | ~annulus_mask.data.astype(np.bool))
+      
       if method == 'mean': bg_val = bg_ima.mean()
       elif method == 'median': bg_val = np.ma.median(bg_ima)
       elif method == 'mode': 
@@ -82,16 +83,58 @@ def photom(ima, pos, radius, r_in=None, r_out=None, method='median'):
     ap.add_column(Column(data=bg_values, name = 'background'))  
     ap['Flux'] = ap['Flux'] - ap['aperture_area']*ap['background']
     return ap, bg_ima
+
+def interpolate_replace(ima, pos, size = 10):
+    kernel = Box2DKernel(3, mode='center')
+    ima_ = ima
+    for x,y in pos:
+        x= int(round(x))
+        y= int(round(y))
+        ima_[y-size:y+size,x-r_out:x+size] = interpolate_replace_nans(ima[y-size:y+size,x-r_out:x+size].filled(), kernel)
+        
+        if False:
+          print x,y
+          plt.figure(35); plt.clf(); plt.ioff()
+          fig, (ax0,ax1) = plt.subplots(ncols=2, nrows=1, num=33)
+          ax0.imshow(ima[y-size:y+size,x-r_out:x+size])
+          ax1.imshow(ima_[y-size:y+size,x-r_out:x+size])
+          plt.show()
+    return ima_
+
+def centroid_method(method):
+    if method == '2dGauss':
+        x, y = 'X_CENTER_Gauss', 'Y_CENTER_Gauss'
+    elif method == '1dGauss':
+        x, y = 'X_CENTER_Gauss1D', 'Y_CENTER_Gauss1D'
+    elif method == 'bary':
+        x, y = 'X_CENTER_Bary Y_CENTER_Bary'
+    return x,y
 ##################################################################
 
-dates       = ['170420', '170428'][:1]
+#dates       = ['170420', '170428']
+dates       = ['170409', '170410', '170411', '170415', '170416', '170417', 
+             '170418', '170419', '170420', '170421', '170422', '170424', 
+             '170428', '170501', '170502']
 band        = 'k'
 flat_type   = 'c'
 object_name = 'WASP-103'
 file_open_mode = 'w'
 file_format = 'ascii'
 
-__root__ = "~/gdrive/WorkV2/IRSF/Observations 2017"
+#aperture photometry params
+r_ap    = 5.0
+r_in    = 15
+r_out   = 24
+method  = 'mode'
+
+#centroid method
+x,y     = centroid_method('2dGauss')
+
+
+#__root__ = "~/gdrive/WorkV2/IRSF/Observations 2017"
+__root__ = "~/Documenti/gdrive/WorkV2/IRSF/Observations 2017"
+photom_output_dir = 'data/photometry_180115'
+
 workbook_fn = os.path.join(__root__,  'obslog_2017_jeni.xlsx')
 
 flat_fn = os.path.join('data/flats/new_flats', '{:s}{:s}flat.fits'.format(band, flat_type))
@@ -102,6 +145,7 @@ fig, ax0 = plt.subplots(nrows = 1, ncols = 1, num=fignum)
 
 
 for date in dates:
+  print "\nDate " + date
 #1# read frames
   raw_data_path = os.path.join(__root__, 'data', 'raw', date, 'rawdata')
   fr_tab = irsf.lib.get_frames_flags(os.path.expanduser(workbook_fn), 
@@ -117,9 +161,9 @@ for date in dates:
   
       break
     
-  for fr in fr_tab['Frame'][:3]:  
-    print "Frame {:04d}\r".format(fr),
-#3# read sci frame  
+  for fr in fr_tab['Frame']:
+    print "Frame {:04d} \r".format(fr),
+#3# read sci frame
     raw_fn = os.path.expanduser(os.path.join(raw_data_path,
               '{:s}{:s}_{:04d}.fits.fz'.format(band, date, fr)))
     
@@ -141,13 +185,32 @@ for date in dates:
     if not os.path.isfile(sourcelist_fn): continue
     source_list = ascii.read(sourcelist_fn, header_start=0, comment='=')
     
-    positions = np.asanyarray(source_list['X_CENTER_Gauss', 'Y_CENTER_Gauss']).tolist()
-    photometry, itmp = photom(ima, positions, 5.0, r_in = 10, r_out=20, method='mode')
+    positions = np.asanyarray(source_list[x, y]).tolist()
+
+#interpolate over bad pixels in apertures
+    ima = interpolate_replace(ima, positions, r_out)
+
+#5# perform photometry
+    id_ = np.asanyarray(source_list['2MASS']).tolist()
+    photometry, itmp = photom(ima, positions, r_ap , r_in = r_in, r_out=r_out, method=method)
+    photometry.add_column(Column(data=id_, name = '2MASS'))
+
+    info_names = ['DATE','Frame', 'Band', 'MJD', 'EXPOS', 'Airmass', 'Humidity', 'Pressure', 'Aperture', 'method']
+    info_dtype = ['S6',  'i4' , 'S1',  np.float,  np.float,  np.float,  np.float,  np.float, np.float, 'S10']
+    info_table = Table(names = info_names, dtype = info_dtype)
+    info_table.add_row((date, fr, band, hdr['MJD'], hdr['EXPOS'], hdr['AIRMASS'], hdr['HUMIDITY'], hdr['PRESSURE'], r_ap, method))
+
     for source in source_list:
-      with open(source['2MASS'], file_open_mode) as fs:
-        keys = ['2MASS', 'X_CENTER_Gauss', 'Y_CENTER_Gauss']
+      with open(os.path.join(os.path.expanduser(__root__),photom_output_dir, source['2MASS']+'_'+method+'_r'+str(r_ap)), 
+                file_open_mode) as fs:
+        keys = ['2MASS', x, y]
         names = ['2MASS', 'X', 'Y']
         output_table = Table([[source[key]] for key in keys], names=names)
+        phot_keys = [ 'Flux', 'aperture_sum', 'aperture_area', 'aperture_badpix', 'background']
+        for i in info_names:
+            output_table.add_column(Column(data = info_table[i], name = i))
+        for k in phot_keys:
+            output_table.add_column(Column(data=photometry[k][np.where(photometry['2MASS']==source['2MASS'])[0]], name = k))
         output_table.write(fs, format=file_format, delimiter=',')
 
     file_open_mode = 'a'
@@ -155,14 +218,8 @@ for date in dates:
 
 # # combile table
     
-    
-
-    
     vmin, vmax = np.percentile(ima.flatten(), (5, 99.))
     im = ax0.imshow(ima.filled(), interpolation='none', cmap='gist_heat', 
                    vmin=vmin, vmax=vmax, origin='lower')
     ax0.format_coord = irsf.lib.Formatter(im)
-    ax0.grid()
-
-  
-  
+    ax0.grid() 
